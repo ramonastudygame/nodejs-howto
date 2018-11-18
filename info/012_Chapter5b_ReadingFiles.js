1333567061360f6517d994101a251c9faf95a0a0
RamonaStudyGame <ramonastudygame@gmail.com>
2018-11-17 23:06:18 -0800

    Study progress

--- info/012_Chapter5b_ReadingFiles.js
+++ info/012_Chapter5b_ReadingFiles.js
@@ -1,30 +1,36 @@
 var fs = require('fs');
-var contents = fs.readFileSync('./lib/Person.js');
-console.log(contents);//you must give UTF-8 encoding for it to read properly
+//var contents = fs.readFileSync('./lib/Person.js');
+//console.log(contents);//you must give UTF-8 encoding for it to read properly
 
-var contents = fs.readFileSync('./lib/Person.js', "UTF-8");//using UTF-8 encoding so you can actually see the contents
-console.log(contents);
+//var contents = fs.readFileSync('./lib/Person.js', "UTF-8");//using UTF-8 encoding so you can actually see the contents
+//console.log(contents);
 
 
-fs.readFile('./lib/Person.js', "UTF-8", function(err, contents){
-	if(err){
-		console.log(err);
-	}
-	console.log(contents);
-});
+// fs.readFile('./lib/Person.js', "UTF-8", function(err, contents){
+// 	if(err){
+// 		console.log(err);
+// 	}
+// 	console.log("My contents:::",contents);
+// });
 
-var fs = require('fs');
+//var fs = require('fs');
 var path = require('path');
 fs.readdir("/.lib", function(err,files){
-	files.forEach(function(fileName){
+	
+	files.forEach(function(fileName) {
 		var file = path.join(__dirname, "lib", fileName);
 		var stats = fs.statSync(file);
+		
 		if (stats.isFile() && fileName !== ".DS_Store"){
+			
 			fs.readFile(file,"UTF-8", function(err,contents){
 				console.log(contents);
 			});
-		}
+
+		};
 	});
+
 });
 
-//THE ABOVE CODE CONTAINS ERRORS SO PLEASE REVISIT
+//THE ABOVE CODE CONTAINS ERRORS, its mostly correct, but forEach undefined error, 
+//please fix and REVISIT
