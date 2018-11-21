eeb83164d8cc420c22ffeff18dc0e72dec054c7e
RamonaStudyGame <ramonastudygame@gmail.com>
2018-11-20 20:58:24 -0800

    Study progress

--- info/practice.js
+++ info/practice.js
@@ -1,3 +1,22 @@
-var path = require("path");
-console.log(`Hi world : ${path.basename(__filename)}`)
-console.log(`Hi there from:${path.basename(__dirname)}`)
+/*var path = require("path");
+console.log(`Hi world : ${path.basename(__filename)}`);
+console.log(`Hi there from:${path.basename(__dirname)}`);
+process.stdout.write("my pony lies over the ocean");
+*/
+//Notice ^ example process.stdout.write acts similar to console.log
+
+/*
+var util = require('util');//will also add a timestamp
+var v8 = require('v8');//v8 will also give memory information
+var dirUploads = path.join(__dirname, 'www', 'files', 'uploads');
+console.log(dirUploads);
+util.log(dirUploads);
+util.log(v8.getHeapStatistics());
+*/
+
+//Readline module allows us to ask questions of our Terminal User. Wraparound of Standard Input Output objects, prompt qns, save answers.
+/*
+var readline = require('readline');
+var rl = readline.createInterface(process.stdin, process.stdout);
+*/
+
